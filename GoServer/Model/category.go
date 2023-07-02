dc206bf8b10d213700fb0970f3b6b370b1ef7127
)`(-@_.+_^*__*^ <55359898+11-aryan@users.noreply.github.com>
2023-07-03 00:54:53 +0530

    Update category.go

--- GoServer/Model/category.go
+++ GoServer/Model/category.go
@@ -1,12 +1,11 @@
 package Model
 
 type Category struct {
-	Id        		string      				`json:"_id" bson:"_id"`
-	CategoryName	string 						`json:"categoryName" bson:"categoryName"`
-	Brands        	[]string    				`json:"brands" bson:"brands"`
-	Colors        	[]string    				`json:"colors" bson:"colors"`	
-	PriceRanges 	[]string					`json:"priceRanges" bson:"priceRanges"`
-	SellerIds 		[]string					`json:"sellerIds" bson:"sellerIds"`
-	// Properties 		bson.M						`json:"properties" bson:"properties"`
-	Properties 		map[string][]interface{}	`json:"properties" bson:"properties"`
+	Id        		  string      				      `json:"_id" bson:"_id"`
+	CategoryName	  string 						        `json:"categoryName" bson:"categoryName"`
+	Brands        	[]string    				      `json:"brands" bson:"brands"`
+	Colors        	[]string    				      `json:"colors" bson:"colors"`	
+	PriceRanges 	  []string					        `json:"priceRanges" bson:"priceRanges"`
+	SellerIds 		  []string					        `json:"sellerIds" bson:"sellerIds"`
+	Properties 		  map[string][]interface{}	`json:"properties" bson:"properties"`
 }
