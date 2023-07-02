a44f67cb35cd76c947d8b8a07d69335fcd1dff72
)`(-@_.+_^*__*^ <55359898+11-aryan@users.noreply.github.com>
2023-07-03 00:53:46 +0530

    Update cart.go

--- GoServer/Model/cart.go
+++ GoServer/Model/cart.go
@@ -4,5 +4,5 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Cart struct {
 	Id        		primitive.ObjectID      `json:"_id" bson:"_id"`
-	CartItems		[]CartItem	`json:"cartItems" bson:"cartItems"`	
+	CartItems		  []CartItem	            `json:"cartItems" bson:"cartItems"`	
 }
