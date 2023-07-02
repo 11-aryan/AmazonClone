d8339695fbeac779a654ca99a3404dc9992d9eab
)`(-@_.+_^*__*^ <55359898+11-aryan@users.noreply.github.com>
2023-07-03 00:58:04 +0530

    Update seller.go

--- GoServer/Model/seller.go
+++ GoServer/Model/seller.go
@@ -1,15 +1,15 @@
 package Model
 
 type Seller struct {
-	Id        		string      `json:"_id" bson:"_id"`
-	FirstName 		string   `json:"firstName" bson:"firstName" `
-	LastName  		string   `json:"lastName" bson:"lastName" `
-	Email     		string   `json:"email" bson:"email" `
-	Password  		string   `json:"password" bson:"password" `
-	MobileNumber    string   `json:"mobileNumber" bson:"mobileNumber"`
-	DateOfBirth     string   `json:"dateOfBirth" bson:"dateOfBirth"`
-	Role      		string   `json:"role" bson:"role"`
-	ProductsListedIds  []string `json:"productsListedIds" bson:"productsListesdIds"`
-	ProductsSoldIds  []string `json:"ProductsSoldIds" bson:"ProductsSoldIds"`
+	Id        		        string     `json:"_id" bson:"_id"`
+	FirstName 		        string     `json:"firstName" bson:"firstName" `
+	LastName  		        string     `json:"lastName" bson:"lastName" `
+	Email     		        string     `json:"email" bson:"email" `
+	Password  		        string     `json:"password" bson:"password" `
+	MobileNumber          string     `json:"mobileNumber" bson:"mobileNumber"`
+	DateOfBirth           string     `json:"dateOfBirth" bson:"dateOfBirth"`
+	Role      		        string     `json:"role" bson:"role"`
+	ProductsListedIds     []string   `json:"productsListedIds" bson:"productsListesdIds"`
+	ProductsSoldIds       []string   `json:"ProductsSoldIds" bson:"ProductsSoldIds"`
 }
 
