1f0c50aea49e82a2c2b96b1158fcb9bc7b149d8a
)`(-@_.+_^*__*^ <55359898+11-aryan@users.noreply.github.com>
2023-07-03 00:54:18 +0530

    Update cartItem.go

--- GoServer/Model/cartItem.go
+++ GoServer/Model/cartItem.go
@@ -1,9 +1,8 @@
 package Model
 
 type CartItem struct {
-	// Id        			string      `json:"_id" bson:"_id"`
-	ProductId        	string      `json:"productId" bson:"productId"`
-	CustomerId        	string      `json:"customerId" bson:"customerId"`
-	QuantityInCart 		int 		`json:"quantityInCart" bson:"quantityInCart"`
-	SelectedForBuying 	bool		`json:"selectedForBuying" bson:"selectedForBuying"`
+	ProductId        	    string      `json:"productId" bson:"productId"`
+	CustomerId        	  string      `json:"customerId" bson:"customerId"`
+	QuantityInCart 		    int 		    `json:"quantityInCart" bson:"quantityInCart"`
+	SelectedForBuying 	  bool		    `json:"selectedForBuying" bson:"selectedForBuying"`
 }
