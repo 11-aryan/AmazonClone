ce42baf3cb092027aa62fbc10faebde8e8083bfa
)`(-@_.+_^*__*^ <55359898+11-aryan@users.noreply.github.com>
2023-07-03 00:56:41 +0530

    Update payment.go

--- GoServer/Model/payment.go
+++ GoServer/Model/payment.go
@@ -1,12 +1,12 @@
 package Model
 
 type Payment struct {
-	Id        			string      `json:"_id" bson:"_id"`
+	Id        			    string      `json:"_id" bson:"_id"`
 	PaymentMethod       string      `json:"paymentMethod" bson:"paymentMethod"`
-	TotalAmount 		int 		`json:"totalAmount" bson:"totalAmount"`	
-	UserId        		string      `json:"userId" bson:"userId"`
-	CardNumber			string   	`json:"cardNumber" bson:"cardNumber"`
-	CardExpiryDate		string 		`json:"cardExpiryDate" bson:"cardExpiryDate"`
-	NameOnCard			string   	`json:"nameOnCard" bson:"nameOnCard"`
-	UpiId		    	string   	`json:"upiId" bson:"upiId"`
+	TotalAmount 		    int 		    `json:"totalAmount" bson:"totalAmount"`	
+	UserId        		  string      `json:"userId" bson:"userId"`
+	CardNumber			    string   	  `json:"cardNumber" bson:"cardNumber"`
+	CardExpiryDate		  string 		  `json:"cardExpiryDate" bson:"cardExpiryDate"`
+	NameOnCard			    string   	  `json:"nameOnCard" bson:"nameOnCard"`
+	UpiId		    	      string   	  `json:"upiId" bson:"upiId"`
 }
