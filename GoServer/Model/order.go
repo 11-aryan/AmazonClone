f459403984a8e2686a4d797f437cabdc25b28a94
)`(-@_.+_^*__*^ <55359898+11-aryan@users.noreply.github.com>
2023-07-03 00:56:08 +0530

    Update order.go

--- GoServer/Model/order.go
+++ GoServer/Model/order.go
@@ -1,15 +1,14 @@
 package Model
 
 type Order struct {
-	Id        			string      `json:"_id" bson:"_id"`
+	Id        			    string      `json:"_id" bson:"_id"`
 	CustomerId        	string      `json:"customerId" bson:"customerId"`
-	// SellerId 			string 		`json:"sellerId" bson:"sellerId"`	
-	ProductId        	string      `json:"productId" bson:"productId"`
-	OrderQuantity		int			`json:"orderQuantity" bson:"orderQuantity"`
-	OrderedDate			string   	`json:"orderedDate" bson:"orderedDate"`
-	DeliveredDate		string   	`json:"deliveredDate" bson:"deliveredDate"`
-	Status		    	string   	`json:"status" bson:"status"`
+	ProductId        	  string      `json:"productId" bson:"productId"`
+	OrderQuantity		    int			    `json:"orderQuantity" bson:"orderQuantity"`
+	OrderedDate			    string   	  `json:"orderedDate" bson:"orderedDate"`
+	DeliveredDate		    string   	  `json:"deliveredDate" bson:"deliveredDate"`
+	Status		    	    string   	  `json:"status" bson:"status"`
 	ShippingAddress    	Address   	`json:"shippingAddress" bson:"shippingAddress"`
-	PaymentDetails		Payment   	`json:"paymentDetails" bson:"paymentDetails"` 
-	TotalAmount 		int 		`json:"totalAmount" bson:"totalAmount"`
+	PaymentDetails		  Payment   	`json:"paymentDetails" bson:"paymentDetails"` 
+	TotalAmount 		    int 		    `json:"totalAmount" bson:"totalAmount"`
 }
