89b6eeca81a0bcf0a4704bc94a643effb7891f1e
)`(-@_.+_^*__*^ <55359898+11-aryan@users.noreply.github.com>
2023-07-03 00:55:38 +0530

    Update customer.go

--- GoServer/Model/customer.go
+++ GoServer/Model/customer.go
@@ -1,17 +1,17 @@
 package Model
 
 type Customer struct {
-	Id        						string      `json:"_id" bson:"_id"`
-	FirstName 						string   	`json:"firstName" bson:"firstName" `
-	LastName  						string   	`json:"lastName" bson:"lastName" `
-	Email     						string   	`json:"email" bson:"email" `
-	Password  						string   	`json:"password" bson:"password" `
-	MobileNumber    				string   	`json:"mobileNumber" bson:"mobileNumber"`
-	DateOfBirth     				string   	`json:"dateOfBirth" bson:"dateOfBirth"`
-	Role      						string   	`json:"role" bson:"role"` 
-	ProductsPurchased 				[]Order 	`json:"productsPurchased" bson:"productsPurchased"`
-	Addresses 		 				[]Address 	`json:"addresses" bson:"addresses"`
-	FriendRequests 					[]string 	`json:"friendRequests" bson:"friendRequests"`
-	Friends 						[]string 	`json:"friends" bson:"friends"`
-	FriendProductRecommendations 	[]string 	`json:"friendProductRecommendations" bson:"friendProductRecommendations"`
+	Id        						        string      `json:"_id" bson:"_id"`
+	FirstName 						        string   	  `json:"firstName" bson:"firstName" `
+	LastName  						        string   	  `json:"lastName" bson:"lastName" `
+	Email     						        string   	  `json:"email" bson:"email" `
+	Password  						        string   	  `json:"password" bson:"password" `
+	MobileNumber    				      string   	  `json:"mobileNumber" bson:"mobileNumber"`
+	DateOfBirth     				      string   	  `json:"dateOfBirth" bson:"dateOfBirth"`
+	Role      						        string   	  `json:"role" bson:"role"` 
+	ProductsPurchased 				    []Order 	  `json:"productsPurchased" bson:"productsPurchased"`
+	Addresses 		 				        []Address 	`json:"addresses" bson:"addresses"`
+	FriendRequests 					      []string 	  `json:"friendRequests" bson:"friendRequests"`
+	Friends 						          []string 	  `json:"friends" bson:"friends"`
+	FriendProductRecommendations 	[]string 	  `json:"friendProductRecommendations" bson:"friendProductRecommendations"`
 }
