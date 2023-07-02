3587a4a1a789e8d53710a78613cfce6d4bd7c812
)`(-@_.+_^*__*^ <55359898+11-aryan@users.noreply.github.com>
2023-07-03 00:52:48 +0530

    Update product.go

--- GoServer/Model/product.go
+++ GoServer/Model/product.go
@@ -2,18 +2,18 @@ package Model
 
  
 type Product struct {
-	Id        			string      `json:"_id" bson:"_id"`
-	ProductName			string 		`json:"productName" bson:"productName"`
-	AboutProduct 		string 		`json:"aboutProduct" bson:"aboutProduct"`
-	SellerId			string		`json:"sellerId" bson:"sellerId"`
-	Brand				string		`json:"brand" bson:"brand"`
-	MaxRetailPrice		int 		`json:"maxRetailPrice" bson:"maxRetailPrice"`
-	SellingPrice		int			`json:"sellingPrice" bson:"sellingPrice"`
-	Quantity			int 		`json:"quantity" bson:"quantity"`
-	UnitsSold			int 		`json:"unitsSold" bson:"unitsSold"`
-	ProductCategories	[]string 	`json:"productCategories" bson:"productCategories"`
-	AverageRating		int 		`json:"averageRating" bson:"averageRating"`
-	ProductImages 		[]string 	`json:"productImages" bson:"productImages"`
-	ProductProperties 	[]string 	`json:"productProperties" bson:"productProperties"`
-	Reviews				[]Review	`json:"reviews" bson:"reviews"`
+  Id        			    string      `json:"_id" bson:"_id"`
+	ProductName			    string 		  `json:"productName" bson:"productName"`
+	AboutProduct 		    string 		  `json:"aboutProduct" bson:"aboutProduct"`
+	SellerId			      string		  `json:"sellerId" bson:"sellerId"`
+	Brand				        string		  `json:"brand" bson:"brand"`
+	MaxRetailPrice		  int 		    `json:"maxRetailPrice" bson:"maxRetailPrice"`
+	SellingPrice		    int			    `json:"sellingPrice" bson:"sellingPrice"`
+	Quantity			      int 		    `json:"quantity" bson:"quantity"`
+	UnitsSold			      int 		    `json:"unitsSold" bson:"unitsSold"`
+	ProductCategories	  []string 	  `json:"productCategories" bson:"productCategories"`
+	AverageRating		    int 		    `json:"averageRating" bson:"averageRating"`
+	ProductImages 		  []string 	  `json:"productImages" bson:"productImages"`
+	ProductProperties   []string 	  `json:"productProperties" bson:"productProperties"`
+	Reviews				      []Review	  `json:"reviews" bson:"reviews"`
 }
